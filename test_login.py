a95d825a068e19d088de05c0ed06cbe751e520f5
a.kolomeytsev <a.kolomeytsev@mac360rds.rambler.ramblermedia.com>
2017-10-23 16:56:47 +0300

    Up!

--- test_login.py
+++ test_login.py
@@ -1,31 +1,22 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+from group import Group
+from Application import Application
+import pytest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test_login(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def test_test_login(self):
-        success = True
-        wd = self.wd
-        wd.get("https://www.rambler.ru/")
-        wd.find_element_by_link_text("Войти").click()
-        wd.find_element_by_link_text("qwe qwe").click()
-        wd.find_element_by_link_text("Выйти").click()
-        self.assertTrue(success)
-    
-    def tearDown(self):
-        self.wd.quit()
 
-if __name__ == '__main__':
-    unittest.main()
+def test_fixber(app):
+    app.login(username="ultras", password="KzkMK")
+    app.edit_product_field(Group(product_kind="TeamCity Redmine Microsoft Visual Studio Python Python"))
+    app.logout()
+
+
+def test_fixber_another_data(app):
+    app.login(username="ultras", password="KzkMK")
+    app.edit_product_field(Group("rambler"))
+    app.logout()
